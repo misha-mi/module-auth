8a4a66804e88541a5cc85fe2a04992c60869a96b
misha <misha.saighanov@mail.ru>
2023-11-15 12:31:02 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FormAuth from "./components/form-auth/form-auth";
+
+function App() {
+  return (
+    <div className="App">
+      <FormAuth/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/form-auth/form-auth.jsx
@@ -0,0 +1,13 @@
+import { useForm } from "react-hook-form"
+
+const FormAuth = () => {
+    const {handleSubmit, control} = useForm();
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FormAuth;
