3be435b8ce27940778344f7f9bbec1d61a13f765
misha <misha.saighanov@mail.ru>
2023-11-15 13:23:22 +0600

    create authorization form

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import FormAuth from "./component/form-auth/form-auth";
+import "./App.sass"
+
+function App() {
+  return (
+    <div className="app">
+      <FormAuth/>
+    </div>
+  );
+}
+
+export default App;
